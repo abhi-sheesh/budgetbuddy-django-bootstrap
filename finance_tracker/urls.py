aae863bc4ab7f264ec88eb618281be40a115a06c
abhishesh <abhisheshbasnet@gmail.com>
2025-05-24 02:05:37 +0545

    signup

--- finance_tracker/urls.py
+++ finance_tracker/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     # Authentication URLs
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html', redirect_authenticated_user = True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    
+    path('signup/', views.signup, name='signup'),
     # Include tracker app URLs
     path('', include('tracker.urls')),
 ]
