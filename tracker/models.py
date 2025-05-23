652a471f978e0ae3255f05257a5fb6eefe19e3c9
abhishesh <abhisheshbasnet@gmail.com>
2025-05-24 01:28:21 +0545

    validation error

--- tracker/models.py
+++ tracker/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
 from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
